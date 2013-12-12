95b0f8dbf81fefa0a20f5ce76dd8002b5d3d28f5
Andrew Mason <andrew.mason@guardian.co.uk>
2013-12-12 16:54:02 +0000

    added views

---
+++ src/js/assets.js
@@ -0,0 +1,18 @@
+/**
+ * Created by andrew on 12/12/13.
+ */
+var JOURNEY = JOURNEY || {};
+JOURNEY.assets = {
+    start: {
+        img: 'http://upload.wikimedia.org/wikipedia/commons/8/81/Waves_lajolla.jpg',
+        html: JOURNEY.views.start
+    },
+    airport: {
+        img: 'http://www.sierraexpressmedia.com/wp-content/uploads/2010/11/Lungi-Terminal.jpg',
+        html: ''
+    },
+    passort: {
+        img: 'http://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Indonesian_passport_first_page_and_inside_cover.jpg/1024px-Indonesian_passport_first_page_and_inside_cover.jpg',
+        html: JOURNEY.views.passport
+    }
+};
